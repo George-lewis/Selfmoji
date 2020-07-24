0319efe5e6cd1e8c2f9db97ddcacc7b06b1c423e
George Lewis <georgejeffreylewis@gmail.com>
2020-07-24 22:13:00 +0000

    lint

--- selfmoji/main.py
+++ selfmoji/main.py
@@ -1,6 +1,10 @@
-import discord, crayons, os, re
-from discord.ext import commands
+import os
+import re
 from configparser import ConfigParser
+
+import crayons
+import discord
+from discord.ext import commands
 
 bot = commands.Bot(command_prefix="``", self_bot=True)
 
@@ -8,27 +12,31 @@ emoji = {}
 
 config = ConfigParser()
 
+
 def save_emojis():
-    with open('emojis.dict', 'w') as file:
+    with open("emojis.dict", "w") as file:
         for k, v in emoji.items():
             file.write(f"{k} : {v}\n")
 
+
 def read_emojis():
-    with open('emojis.dict', 'r') as file:
+    with open("emojis.dict", "r") as file:
         for line in file:
-            k, v = line.strip().split(' : ')
+            k, v = line.strip().split(" : ")
             emoji[k] = v
 
+
 def token() -> str:
-    if token := os.getenv('DISCORD_TOKEN'):
+    if token := os.getenv("DISCORD_TOKEN"):
         print(crayons.green("Loading token from environment"))
         return token
-    elif os.path.isfile('TOKEN'):
+    elif os.path.isfile("TOKEN"):
         print(crayons.green("Loading token from file"))
-        with open('TOKEN', 'r') as file:
+        with open("TOKEN", "r") as file:
             return file.read().strip()
     else:
         raise ValueError("Bruh")
+
 
 def main():
 
@@ -50,21 +58,24 @@ def main():
         print(crayons.red("SAVING EMOJIS"))
         save_emojis()
 
+
 @bot.command()
 async def add(ctx, name, link):
     print(crayons.yellow(f"Registering emoji [{name}] with [{link}]"))
     emoji[name.strip()] = link.strip()
     await ctx.message.delete()
 
+
 @bot.event
 async def on_ready():
     print(crayons.green("ready!"))
+
 
 @bot.event
 async def on_message(message):
     if message.author != bot.user:
         return
-    
+
     if not re.match("`.+`", message.content):
         await bot.process_commands(message)
         return
@@ -76,7 +87,7 @@ async def on_message(message):
 
     e = emoji[content] + "&size=32"
 
-    if config['selfmoji'].getboolean('edit'):
+    if config["selfmoji"].getboolean("edit"):
 
         await message.edit(content=e)
 
@@ -85,6 +96,7 @@ async def on_message(message):
         await message.delete()
 
         await message.channel.send(e)
+
 
 if __name__ == "__main__":
     main()
